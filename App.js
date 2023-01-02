c9caa009a0c9abd1bc3b38010605a234de516ecb
Jscorz <jkscorzafava@gmail.com>
2023-01-02 15:39:44 -0700

    installed drawer package and configured options for it

--- App.js
+++ App.js
@@ -1,5 +1,27 @@
-import { Text } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return <Text>Todo...</Text>;
+	return (
+		<NavigationContainer>
+			<Drawer.Navigator>
+				<Drawer.Screen
+					name='Welcome'
+					component={WelcomeScreen}
+					options={{
+						headerStyle: { backgroundColor: "#3c0a6b" },
+						headerTintColor: "white",
+						drawerLabel: "Welcome Screen",
+						drawerActiveBackgroundColor: "#f0e1ff",
+						drawerActiveTintColor: "#3c0a6b",
+					}}
+				/>
+				<Drawer.Screen name='User' component={UserScreen} />
+			</Drawer.Navigator>
+		</NavigationContainer>
+	);
 }
